832d0ebb9b2c4535166985f4ac5dc44cfa5d2820
YoloMao <a83506677@126.com>
2021-10-13 10:35:28 +0800

    feat: release 0.2.2

--- GrowingToolsKit/Core/GrowingRealToolsKit.m
+++ GrowingToolsKit/Core/GrowingRealToolsKit.m
@@ -23,7 +23,7 @@
 #import "GrowingTKEntryWindow.h"
 
 NSString *const GrowingToolsKitName = @"GrowingToolsKit";
-NSString *const GrowingToolsKitVersion = @"0.2.1";
+NSString *const GrowingToolsKitVersion = @"0.2.2";
 
 NSString *const GrowingTKHomeWillShowNotification = @"GrowingTKHomeWillShowNotification";
 NSString *const GrowingTKHomeShouldHideNotification = @"GrowingTKHomeShouldHideNotification";
