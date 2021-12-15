76cab1db73197277dfe398747eaa41765d99a003
YoloMao <a83506677@126.com>
2021-12-15 16:00:20 +0800

    feat: release 1.0.2

--- GrowingToolsKit.podspec
+++ GrowingToolsKit.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'GrowingToolsKit'
-  s.version          = '1.0.1'
+  s.version          = '1.0.2'
   s.summary          = 'GrowingToolsKit for iOS GrowingIO SDK'
   s.description      = <<-DESC
 TODO: Add long description of the pod here.
--- GrowingToolsKit/Core/GrowingRealToolsKit.m
+++ GrowingToolsKit/Core/GrowingRealToolsKit.m
@@ -23,7 +23,7 @@
 #import "GrowingTKEntryWindow.h"
 
 NSString *const GrowingToolsKitName = @"GrowingToolsKit";
-NSString *const GrowingToolsKitVersion = @"1.0.1";
+NSString *const GrowingToolsKitVersion = @"1.0.2";
 
 NSString *const GrowingTKHomeWillShowNotification = @"GrowingTKHomeWillShowNotification";
 NSString *const GrowingTKHomeShouldHideNotification = @"GrowingTKHomeShouldHideNotification";
