82a527a5811d97ccabb2a245939048e9653d39fc
YoloMao <a83506677@126.com>
2023-10-11 11:26:21 +0800

    fix: update spm dependency version

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/growingio/growingio-sdk-ios-performance-ext.git",
-            "0.0.15" ..< "1.0.0"
+            "1.0.0" ..< "2.0.0"
         ),
     ],
     targets: [
